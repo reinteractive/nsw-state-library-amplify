9b244f74e8ed31e4e81a4df0d05ff5bced11f036
sameera207 <sameera207@gmail.com>
2018-07-25 11:37:29 +1000

    tags update issue fixed, dropdown UI fixes

--- app/models/sort_list.rb
+++ app/models/sort_list.rb
@@ -1,20 +1,21 @@
 # sorting list for the homepage dropown
 class SortList
-
   def self.list
     arr = []
-    {
-      random: 'Random',
-      title_asc: 'Title (A to Z)',
-      title_desc: 'Title (Z to A)',
-      percent_completed_desc: 'Completeness (most to least)',
-      percent_completed_asc: 'Completeness (least to most)',
-      duration_asc: 'Duration (short to long)',
-      duration_desc: 'Duration (long to short)'
-    }.each do |key, value|
+    options.each do |key, value|
       arr << OpenStruct.new(id: key, title: value)
     end
     arr
   end
 
+  def self.options
+    {
+      title_asc: "Title (A to Z)",
+      title_desc: "Title (Z to A)",
+      percent_completed_desc: "Completeness (most to least)",
+      percent_completed_asc: "Completeness (least to most)",
+      duration_asc: "Duration (short to long)",
+      duration_desc: "Duration (long to short)",
+    }
+  end
 end
