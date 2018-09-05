60fbb30cf0476a18fc4ff896f059dc35ef7e5a3d
sameera207 <sameera207@gmail.com>
2018-09-05 12:34:33 +1000

    code cleanup

--- config/routes.rb
+++ config/routes.rb
@@ -42,9 +42,7 @@ Rails.application.routes.draw do
   match 'page/preview/:id' => 'page#preview', :via => [:get]
   match 'page/:id' => 'page#show', :via => [:get]
 
-  # match 'dashboard' => 'default#index', :via => [:get]
   match 'transcript_lines/:id/resolve' => 'transcript_lines#resolve', :via => [:post]
-  # match 'search' => 'transcripts#search', :via => [:get]
 
   # admin
   namespace :admin do
